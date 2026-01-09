e8c2d432d667752ff687e9eba4f8fd7850f5fa4d
cpmachado <cpmachado@protonmail.com>
2026-01-09 11:30:20 +0000

    cli args to load file

--- cmd/nofox/main.go
+++ cmd/nofox/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello nofox")
+	filename := ""
+	flag.StringVar(&filename, "f", "", "Input file")
+	flag.Parse()
+
+	if filename == "" {
+		flag.CommandLine.Usage()
+		log.Fatal("missing input file")
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
 }
