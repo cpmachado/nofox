e16cb5fa0d8b9e182def450b0f0b060e52e07129
cpmachado <cpmachado@protonmail.com>
2026-01-25 15:04:21 +0000

    nuke everything

--- cmd/nofox/main.go
+++ cmd/nofox/main.go
@@ -1,20 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/cpmachado/nofox"
-)
-
 func main() {
-	l := nofox.Lexer{TokenMap: nofox.DefaultTokens, Source: os.Stdin}
-
-	tokens := l.Lex()
-
-	for _, token := range tokens {
-		if token.Type != nofox.Ignored {
-			fmt.Println(token.String())
-		}
-	}
 }
