ba7fd0429334ad946b9e92407c2d39af44d72fda
cpmachado <cpmachado@protonmail.com>
2026-01-25 15:00:36 +0000

    Remove read from file

--- cmd/nofox/main.go
+++ cmd/nofox/main.go
@@ -1,36 +1,14 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/cpmachado/nofox"
 )
 
 func main() {
-	filename := ""
-	flag.StringVar(&filename, "f", "", "Input file")
-	flag.Parse()
-
-	if filename == "" {
-		flag.CommandLine.Usage()
-		log.Fatal("missing input file")
-	}
-
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer func() {
-		if err := file.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	l := nofox.Lexer{TokenMap: nofox.DefaultTokens, Source: file}
+	l := nofox.Lexer{TokenMap: nofox.DefaultTokens, Source: os.Stdin}
 
 	tokens := l.Lex()
 
